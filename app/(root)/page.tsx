c8863bb02658ca74ee390f2d7b74f5b1dc53673c
GMKrishna99 <gmohankrishna23@gmail.com>
2024-01-10 12:30:19 +0530

    chore : shadcn UI button component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <h1>Admin Page</h1>
+      <Button>Hello world</Button>
     </>
   );
 }
