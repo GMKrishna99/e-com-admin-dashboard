033fb1c730a79a758d64577936911e44c692187f
GMKrishna99 <gmohankrishna23@gmail.com>
2024-01-13 14:48:11 +0530

    chore : added store modal state and effects

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 
 import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
+/**
+ * Component representing the setup page.
+ * It utilizes the useStoreModal custom hook to manage modal state.
+ * The component triggers the modal to open when it is not already open.
+ *
+ * @returns {JSX.Element} - JSX element representing the SetupPage component.
+ */
 const SetupPage = () => {
-  return (
-    <div className="p-4">
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
-        Children
-      </Modal>
-    </div>
-  );
-};
+  // Using the useStoreModal hook to access modal state and functions.
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
 
+  // Effect to open the modal when it is not already open.
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  // Render the SetupPage component with a simple message.
+  return <div className="p-4">Root Page</div>;
+};
 export default SetupPage;
